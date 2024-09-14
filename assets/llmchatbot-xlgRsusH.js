ba6a8f69a986e721cdc67e98db67429019608905
GitHub Action <action@github.com>
2024-09-14 14:20:07 +0000

    Update documentation

---
+++ assets/llmchatbot-xlgRsusH.js
@@ -0,0 +1,3 @@
+const t={date:"2024-08",title:"LLM Chatbot",url:"https://github.com/jason5ng32/Github-Models-Telegram-Bot",tags:["ChatGPT","Javascript"]},a=`<p>在 Telegram 上使用 Github AI 大模型进行聊天。</p>
+`,e=`在 Telegram 上使用 Github AI 大模型进行聊天。
+`;export{t as attributes,a as html,e as markdown};
