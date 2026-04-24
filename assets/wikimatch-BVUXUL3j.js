1a4437a316d590a30c12f4412a562f097e3a4c52
GitHub Action <action@github.com>
2026-04-24 10:40:32 +0000

    Update documentation

--- assets/wikimatch-BVUXUL3j.js
+++
@@ -1,3 +0,0 @@
-const t={date:"2026",title:"WikiMatch",cover:"wikimatch.jpg",url:"https://wikimatch.app/",tags:["游戏","知识学习","Vibe Coding"]},i=`<p>一款基于 Wikipedia 的多人知识竞技游戏。给定相同的目标词条和相同的随机起始词条，每个人只能通过点击词条页面的链接前进，并最终到达目标词条。先到达目标词条者获胜。</p>
-`,a=`一款基于 Wikipedia 的多人知识竞技游戏。给定相同的目标词条和相同的随机起始词条，每个人只能通过点击词条页面的链接前进，并最终到达目标词条。先到达目标词条者获胜。
-`;export{t as attributes,i as html,a as markdown};
